5df0d5ace2595cbbd016452cc01311730329598e
amigoscode <amigoscode@gmail.com>
2021-10-16 23:41:11 +0100

    examples

---
+++ src/test/java/com/amigoscode/examples/WorkingWithStreams.java
@@ -0,0 +1,26 @@
+package com.amigoscode.examples;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class WorkingWithStreams {
+
+    @Test
+    void steams() {
+        List<String> names = List.of("Amigoscode", "Alex", "Zara");
+        Stream<String> stream = names.stream();
+
+        Stream<String> namesStream = Stream.of("Amigoscode", "Alex", "Zara");
+
+        long count = stream
+                .limit(2).map(null).sorted(null).dropWhile(null)
+                .count();
+
+        String[] namesArray = {};
+        Arrays.stream(namesArray);
+    }
+}
