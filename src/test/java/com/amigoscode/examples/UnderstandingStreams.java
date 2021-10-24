f867b5fb9457e1cf28fe06fe076ef2d358121dc5
amigoscode <amigoscode@gmail.com>
2021-10-24 19:34:44 +0100

    Understanding steams

---
+++ src/test/java/com/amigoscode/examples/UnderstandingStreams.java
@@ -0,0 +1,47 @@
+package com.amigoscode.examples;
+
+import com.amigoscode.beans.Person;
+import com.amigoscode.mockdata.MockData;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UnderstandingStreams {
+
+    @Test
+    void collect() throws IOException {
+        List<String> emails = MockData.getPeople()
+                .stream()
+                .map(Person::getEmail)
+                .collect(
+                        ArrayList::new,
+                        ArrayList::add,
+                        ArrayList::addAll
+                );
+        emails.forEach(System.out::println);
+    }
+
+    @Test
+    public void lazy() throws Exception {
+        System.out.println(
+                MockData.getCars()
+                        .stream()
+                        .filter(car -> {
+                            System.out.println("filter car " + car);
+                            return car.getPrice() < 10000;
+                        })
+                        .map(car -> {
+                            System.out.println("mapping car " + car);
+                            return car.getPrice();
+                        })
+                        .map(price -> {
+                            System.out.println("mapping price " + price);
+                            return price + (price * .14);
+                        })
+                        .collect(Collectors.toList())
+        );
+    }
+}
