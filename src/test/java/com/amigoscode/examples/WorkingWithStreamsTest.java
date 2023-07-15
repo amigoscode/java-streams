90f62a10ec27525b7da1d413bf0fac9b8bc60416
Sylvester Loreto <sylvester.abreu@googlemail.com>
2023-07-15 11:30:21 +0100

    FIXED broken test - Count items in a stream after performing a few operations with streams

--- src/test/java/com/amigoscode/examples/WorkingWithStreamsTest.java
+++ src/test/java/com/amigoscode/examples/WorkingWithStreamsTest.java
@@ -3,7 +3,6 @@ package com.amigoscode.examples;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -11,13 +10,12 @@ class WorkingWithStreamsTest {
 
     @Test
     void count_items_on_stream() {
-        var names = List.of("Amigoscode", "Alex", "Zara");
+        var names = List.of("Amigoscode", "Alex", "Zara", "Sarah", "Alan");
         var stream = names.stream();
 
-        var count = stream.limit(2)
-                .map(null)
-                .sorted(null)
-                .dropWhile(null)
+        var count = stream
+                .filter(name -> name.charAt(0) == 'A')
+                .limit(2)
                 .count();
 
         assertThat(count).isEqualTo(2);
