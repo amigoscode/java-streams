779f17d0d2cfeab68501dabb94b787f776750d10
Sylvester Loreto <sylvester.abreu@googlemail.com>
2023-07-15 12:07:11 +0100

    Get all yellow cars under 20k

--- src/test/java/com/amigoscode/examples/FilteringTest.java
+++ src/test/java/com/amigoscode/examples/FilteringTest.java
@@ -1,26 +1,35 @@
 package com.amigoscode.examples;
 
 import com.amigoscode.beans.Car;
-import com.amigoscode.beans.Person;
 import com.amigoscode.mockdata.MockData;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.OptionalInt;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Filtering {
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class FilteringTest {
 
     @Test
-    public void filter() throws Exception {
-        List<Car> cars = MockData.getCars();
+    void get_all_yellow_cars_under_20k() throws Exception {
+        var cars = MockData.getCars();
+
+        Predicate<Car> price = car -> car.getPrice() < 20_000.00;
+        Predicate<Car> color = car -> car.getColor().equals("Yellow");
+
+        var carsUnder20k = cars.stream()
+                .filter(price)
+                .filter(color)
+                .toList();
+
+        assertThat(carsUnder20k.size()).isEqualTo(9);
+        carsUnder20k.forEach(car ->
+                assertThat(car.getPrice()).isLessThan(20_000.00));
     }
 
     @Test
-    public void dropWhile() throws Exception {
+    void dropWhile() throws Exception {
         System.out.println("using filter");
         Stream.of(2, 4, 6, 8, 9, 10, 12).filter(n -> n % 2 == 0)
                 .forEach(n -> System.out.print(n + " "));
@@ -29,7 +38,7 @@ public class Filtering {
     }
 
     @Test
-    public void takeWhile() throws Exception {
+    void takeWhile() throws Exception {
         // using filter
         System.out.println("using filter");
         Stream.of(2, 4, 6, 8, 9, 10, 12).filter(n -> n % 2 == 0)
@@ -40,22 +49,22 @@ public class Filtering {
     }
 
     @Test
-    public void findFirst() throws Exception {
+    void findFirst() throws Exception {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     }
 
     @Test
-    public void findAny() throws Exception {
+    void findAny() throws Exception {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10};
     }
 
     @Test
-    public void allMatch() throws Exception {
+    void allMatch() throws Exception {
         int[] even = {2, 4, 6, 8, 10};
     }
 
     @Test
-    public void anyMatch() throws Exception {
+    void anyMatch() throws Exception {
         int[] evenAndOneOdd = {2, 4, 6, 8, 10, 11};
     }
 
